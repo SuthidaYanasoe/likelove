e38d37f11b140c3555ec88aadc4c42b4a9fcb0df
YOURNAME <your@email.com>
2020-09-29 09:59:41 +0700

    .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+
+    <HelloWorld msg="We want everyone to join the press and express their feelings as show in the picture." />
+    <hr>
+    <div class="row">
+        <div class="col-6">
+            <img src="https://img.kapook.com/u/2018/Jarosphan/Pet/Cat/166/01.jpg" width="630" height="430">
+            <br />
+            <br />
+            <h3>Persian cat</h3>
+            <h7>อ่อนโยน เข้ากับคนอื่นง่าย ช่างประจบ มีไหวพริบมาก</h7>
+            <h1>{{total1}}</h1>
+            <CsButton @cc="sum" />
+        </div>
+        <div class="col-6">
+            <img src="https://img.kapook.com/u/pirawan/Pet/shutterstock_119848222.jpg" width="630" height="430">
+            <br />
+            <br />
+            <h3>Main Coon cat</h3>
+            <h7>นิสัยขี้อ่อน ผิดกับหน้าตา ขี้เล่น ร่าเริง</h7>
+            <h1>{{total2}}</h1>
+            <CsButton @cc="sum1" />
+        </div>
+        <div class="col-6">
+            <img src="https://img.kapook.com/u/pirawan/Pet/bangol1.jpg" width="630" height="430">
+            <br />
+            <br />
+            <h3>Bengal cat</h3>
+            <h7>เป็นมิตรกับทุกคน ซุกซน ชอบปีนป่าย ชอบเล่นน้ำ</h7>
+            <h1>{{total3}}</h1>
+        </div>
+
+        <div class="col-6">
+            <img src="https://static.trueplookpanya.com/tppy/member/m_545000_547500/545994/cms/images/2018-08-Sep/%E0%B8%A7%E0%B8%B4%E0%B9%80%E0%B8%8A%E0%B8%B5%E0%B8%A2%E0%B8%A3%E0%B8%A1%E0%B8%B2%E0%B8%A8_02.jpg" width="630" height="430">
+            <br />
+            <br />
+            <h3>Siamese cat</h3>
+            <h7>ฉลาด ปราดเปรียว เป็นตัวของตัวเองสูง</h7>
+            <h1>{{total4}}</h1>
+        </div>
+        <div class="col-6">
+            <CsButton @cc="sum2" />
+        </div>
+        <div class="col-6">
+            <CsButton @cc="sum3" />
+        </div>
+
+    </div>
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import CsButton from "./components/CSButton.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        HelloWorld,
+        CsButton,
+
+    },
+    data: function () {
+        return {
+            total1: 0,
+            total2: 0,
+            total3: 0,
+            total4: 0,
+
+        }
+    },
+    methods: {
+        sum(counterValue) {
+            this.total1 = counterValue
+        },
+
+        sum1(counterValue) {
+            this.total2 = counterValue
+
+        },
+
+        sum2(counterValue) {
+            this.total3 = counterValue
+        },
+
+        sum3(counterValue) {
+            this.total4 = counterValue
+        },
+
+    },
+
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #000000;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/CSButton.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+  
+    <button v-on:click="upFunction" class="btn btn-outline-primary">ถูกใจมาก {{lable}}</button>
+    <button v-on:click="downFunction" class="btn btn-outline-danger ml-3">ไม่ถูกใจอย่างแรง {{lable}}</button>
+    <button v-on:click="LoveFunction" class="btn btn-outline-warning ml-3">รักมากมาย {{lable}}</button>
+    <button v-on:click="ResetFunction" class="btn btn-outline-secondary ml-3">รีเซ็ท {{lable}}</button>
+    <br />
+    <br />
+    
+      
+</div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            counter: 0,
+        };
+    },
+    props: {
+        lable: String,
+    },
+    methods: { 
+        upFunction: function () {
+            this.counter += 10;
+            this.$emit("cc", this.counter);
+            
+        },
+
+        downFunction: function () {
+            this.counter += -5;
+            this.$emit("cc", this.counter);
+        },
+
+        LoveFunction: function () {
+            this.counter += 10;
+            this.$emit("cc", this.counter);
+        },
+
+        ResetFunction: function () {
+            this.counter = 0;
+            this.$emit("cc", this.counter);
+        },
+    },
+};
+</script>
+
+<style>
+</style>
